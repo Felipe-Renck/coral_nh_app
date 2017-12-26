2cb99df14172989176a335192b5da1a929c3f810
Felipe Rosa Renck <felipe-renck@hotmail.com>
2017-12-25 22:36:52 -0200

    Initial Commit

---
+++ coralApp/coralApp/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace coralApp
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+            WebView webView = new WebView
+            {
+                Source = new UrlWebViewSource
+                {
+                    Url = "https://www.facebook.com/coraljovemnh/",
+                },
+                VerticalOptions = LayoutOptions.FillAndExpand
+            };
+
+            // Accomodate iPhone status bar.
+            this.Padding = new Thickness(10, Device.OnPlatform(20, 0, 0), 10, 5);
+
+            // Build the page.
+            this.Content = new StackLayout
+            {
+                Children =
+                {
+                    webView
+                }
+            };
+        }
+    }
+}
